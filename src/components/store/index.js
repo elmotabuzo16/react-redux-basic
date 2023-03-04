5b87fd987bcc33fafe7633bbec746ab7b97beb93
Elmo <elmotabuzojr.ust@gmail.com>
2023-03-04 20:03:59 +0800

    working with multiple state properties

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,9 +1,12 @@
 const redux = require('redux');
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
@@ -11,12 +14,21 @@ const counterReducer = (state = { counter: 0 }, action) => {
     return {
       // action after (dot) is any name that you can use. ie., action.value, action.test
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === 'toggle') {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
 
