182d38d21e820f7630d0a3d024202f2e013b9420
Elmo <elmotabuzojr.ust@gmail.com>
2023-03-04 22:41:23 +0800

    reading and dispatching from a new slice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  // state.auth beceause auth is coming from configureStore in store
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </Fragment>
   );
